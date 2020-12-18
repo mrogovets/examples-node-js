8bcd5661b10693ef42e3fb6ae7e3bf2158b1150b
mrogovets <34492317+mrogovets@users.noreply.github.com>
2020-12-18 17:40:06 +0200

    Finish example: body-parser, read URL

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const app = express();
+
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
 
 app.set("view engine", "ejs");
 
@@ -14,12 +17,19 @@ app.get("/about", (req, res) => {
   res.render("about");
 });
 
+app.post("/about", urlencodedParser, (req, res) => {
+  if (!req.body) return res.sendStatus(400);
+  console.log(req.body);
+  res.render("about-success", { data: req.body });
+});
+
 app.get("/news/:id", (req, res) => {
   const obj = {
     title: "News",
     id: 3,
     paragraphs: ["Paragraph", "text/plain", "numbers: 2, 4, 6", 99],
   };
+  console.log(req.query);
   res.render("news", { newsId: req.params.id, obj: obj });
 });
 
